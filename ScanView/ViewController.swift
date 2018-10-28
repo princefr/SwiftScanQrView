1da7ecc45b22b16ff3ecfdcaa0f77bff0501be45
Prince ond <ondonda.prince@hotmail.fr>
2018-10-28 18:48:41 +0100

    Initial Commit

---
+++ ScanView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanView
+//
+//  Created by prince jackes on 28/10/2018.
+//  Copyright © 2018 prince jackes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
